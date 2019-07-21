e6334097636757105f7e5c9c4a8e3027d5f721e0
angular210709 <53136440+angular210709@users.noreply.github.com>
2019-07-21 11:24:09 +0300

    Create start_1.js

---
+++ start_1.js
@@ -0,0 +1,16 @@
+console.log('hello js')
+
+function c()
+{
+  console.log()
+}
+
+c // prints out c function code
+
+const arr = [ 1 , 2, 3 ]
+arr.push(3) // adds in the end
+arr.unshift(1) // adds in the start
+arr.pop() // takes out in the end
+arr.shift() // takes out in the start
+arr.slice(3, 10) // takes out 3-10
+arr.slice(1) // takes out from 1 till end
